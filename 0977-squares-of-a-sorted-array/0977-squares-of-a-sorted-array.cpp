fba9721f2cf2fc281e9ce692463c256a2cfb856a
Wasif Wasim Ahmad Ansari <wasifansari92942@gmail.com>
2026-07-17 12:07:44 +0530

    Time: 4 ms (41.32%), Space: 30.4 MB (39.86%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,72 +1,20 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& a) {
-        int siz=a.size();
-        vector<int> neg;
-        vector<int> pos;
-        // int i;
-        for(int i=0;i<siz;i++)
-        {
-            if(a[i]<0)
-            neg.push_back(a[i]);
-            else
-            pos.push_back(a[i]);
-
-        }
-        if(neg.size()==0) // no negative element
-        {
-            for(int i=0;i<pos.size();i++)
-            pos[i]=pos[i]*pos[i];
-            return pos;
-        }
-        if(pos.size()==0) // no positibve
-        {
-            for(int i=0;i<neg.size();i++)
-            neg[i]=neg[i]*neg[i];
-            reverse(neg.begin(),neg.end());
-            return neg;
-        }
-       
-        int i=0,j=0;
-        int id=0;
-        int n=neg.size();
-        int m=pos.size();
-        vector<int> res(n+m);
-        for(int i=0;i<n;i++)
-        neg[i]=neg[i]*neg[i];
-
-        reverse(neg.begin(),neg.end());
-        for(int i=0;i<m;i++)
-        pos[i]=pos[i]*pos[i];
-        while(i<n and j<m)
-        {
-            if(neg[i]<=pos[j])
-            {
-                res[id]=neg[i];
-                id++;
-                i++;
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> ans(n);
+        int left = 0;
+        int right = n-1;
+        int i;
+        for ( int i=n-1 ; i>=0 ; i-- ) {
+            if(abs(nums[left])>abs(nums[right])) {
+                ans[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                ans[i] = nums[right] * nums[right];
+                right--; 
             }
-            else
-            {
-                res[id]=pos[j];
-                id++;
-                j++;
-            }
-        } // whilwe loop khatam
-        while(i<n)
-        {
-            res[id]=neg[i];
-                id++;
-                i++;
         }
-        while(j<m)
-        {
-            res[id]=pos[j];
-                id++;
-                j++;
-        }
-
-
-        return res;
+        return ans;
     }
 };
