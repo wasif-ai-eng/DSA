eb6147c928f10a8b3eb4a64e3d3dc38e69771603
Wasif Wasim Ahmad Ansari <wasifansari92942@gmail.com>
2026-07-21 18:09:36 +0530

    Time: 26 ms (25.73%), Space: 17.5 MB (55.62%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> result;
+        if (n < 4) return result;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip dup i
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip dup j
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                        // skip duplicates for k and l
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                    else if (sum < target) {
+                        k++;
+                    }
+                    else {
+                        l--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
