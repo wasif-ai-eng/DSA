48b7d5ed76882c47a64c9e459fa27298a9a4af53
Wasif Wasim Ahmad Ansari <wasifansari92942@gmail.com>
2026-07-22 21:00:45 +0530

    Time: 0 ms (100%), Space: 8.2 MB (99.91%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+     
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            if (i >= 0 && j >= 0) {
+                if (s[i] != t[j]) return false;
+            } else {
+                // one string ran out of characters, the other didn't
+                if (i >= 0 || j >= 0) return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+};
