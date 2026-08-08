c4f496ad968595b3cf13afb3d4f1a1b0dbb30cc7
Wasif Wasim Ahmad Ansari <wasifansari92942@gmail.com>
2026-08-08 23:35:06 +0530

    Time: 0 ms (100%), Space: 65.1 MB (70.27%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=0;
+        int fast=0;
+        while(true){
+            slow=nums[slow];
+            fast=nums[fast];
+            fast=nums[fast];
+            if(slow==fast) {
+                slow=0;
+                break;
+            }
+                }
+         
+         while(slow!=fast){
+          slow=nums[slow];
+           fast=nums[fast];
+                }
+                return fast;
+    }
+};
