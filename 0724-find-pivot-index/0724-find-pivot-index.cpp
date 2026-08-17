3e25f2f26969b23fb4ba0c4620c53be3b06595d6
Wasif Wasim Ahmad Ansari <wasifansari92942@gmail.com>
2026-08-17 17:07:00 +0530

    Time: 0 ms (100%), Space: 35.6 MB (97.02%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-
-    int prefix(vector<int>& arr, int i) {
-        if (i == 0)
-            return 0;
-
-        return prefix(arr, i - 1) + arr[i - 1];
-    }
-
-    int suffix(vector<int>& arr, int i, int n) {
-        if (i == n - 1)
-            return 0;
-
-        return suffix(arr, i + 1, n) + arr[i + 1];
-    }
-
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
+        int sum=0,prev=0;
 
-            int left = prefix(nums, i);
-            int right = suffix(nums, i, n);
-
-            if (left == right)
-                return i;
+        for(int i=0;i<nums.size();i++)
+        {
+             sum+=nums[i];
         }
 
+        for(int i=0;i<nums.size();i++)
+        {
+            if(prev==sum-nums[i])
+            return i;
+
+            sum-=nums[i];
+            prev+=nums[i];
+        }
         return -1;
     }
 };
